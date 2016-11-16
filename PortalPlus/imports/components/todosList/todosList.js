d7f547af9d6242b02a0596903f69d6f8ef855664
allidoisace <arroservices@live.com>
2016-11-15 21:25:24 -0800

    General

---
+++ PortalPlus/imports/components/todosList/todosList.js
@@ -0,0 +1,42 @@
+//adds functionality to todosList.html
+
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+
+import template from './todosList.html';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        // Show newest tasks at the top
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    })
+ }
+ addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    // Clear form
+    this.newTask = '';
+  }
+ }
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl] //from class TodosListCtrl
+  });
