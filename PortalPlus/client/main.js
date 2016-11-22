36714d41f7c4d70c6187192edb21fb7340d4be4a
Scott Lindeneau <slindeneau@gmail.com>
2016-11-22 15:46:11 -0800

    Make UI version run in meteor. Place static content appropriately.

--- PortalPlus/client/main.js
+++ PortalPlus/client/main.js
@@ -1,12 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import todosList from '../imports/components/todosList/todosList';
-import semesterList from '../imports/components/semesterList/semesterList'
-import '../imports/startup/accounts-config.js';
 
 angular.module('portal-plus', [
   angularMeteor,
-  todosList.name,
-  semesterList.name,
-  'accounts.ui'
 ]);
