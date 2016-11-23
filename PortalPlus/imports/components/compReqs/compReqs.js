b8bc7eb1d35d2f14eeaa19056362bfc27f9e281d
Scott Lindeneau <slindeneau@gmail.com>
2016-11-22 18:45:48 -0800

    Modifies the competency requirements such that all of them are generated by a ng-repeat command. The data comes from a static list inside the compReqsCtrl. Demonstrates the use of the {{$index}} variable to produce unique ID’s such that the expando js can correctly target the created div and can correctly expand as appropriate.
    Simplifying assumption: Everything is complete.

--- PortalPlus/imports/components/compReqs/compReqs.js
+++ PortalPlus/imports/components/compReqs/compReqs.js
@@ -6,13 +6,29 @@ import template from './compReqs.html';
 
 class compReqsCtrl {
   constructor() {
+    this.reqs = [{text:"COMPETENCY REQUIREMENTS"},
+    {text:"GRADUATION WRITING ASSESSMENT REQUIREMENT"},
+    {text:"AMERICAN INSTITUTIONS REQUIREMENT SATISFIED"},
+    {text:"UNIT, RESIDENCE AND GPA REQUIREMENTS"},
+    {text:"CULTURAL DIVERSITY"},
+    {text:"IVA.  EXPLORATIONS - NATURAL SCIENCES"},
+    {text:"IVC.  EXPLORATIONS - HUMANITIES"},
+    {text:"PREPARATION FOR THE MAJOR"},
+    {text:"MAJOR REQUIREMENTS"},
+    ];
   }
 }
+
+
+
+
+
+
 
 export default angular.module('compReqs', [
   angularMeteor
 ])
   .component('compReqs', {
     templateUrl: 'imports/components/compReqs/compReqs.html',
-    controller: ['$scope', compReqsCtrl] 
+    controller: ['$scope', compReqsCtrl]
   });
